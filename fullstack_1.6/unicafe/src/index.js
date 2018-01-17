6dad6d7afe73a519ba43d7a1bb5410641efaaaf0
RoopeNiemi <32167039+RoopeNiemi@users.noreply.github.com>
2018-01-17 02:32:49 +0200

    tehtavia

--- fullstack_1.6/unicafe/src/index.js
+++ fullstack_1.6/unicafe/src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics=(props)=>{
+  const hyvia=props.stat.hyvä
+  const neutraaleja= props.stat.neutraali
+  const huonoja= props.stat.huono
+  const yhteensa= props.stat.yhteensa
     if(props.stat.yhteensa===0){
         return(
             <p>Yhtään palautetta ei ole annettu</p>
@@ -10,29 +14,39 @@ const Statistics=(props)=>{
     return(
         <div>
             <h3> Statistiikka </h3>
+            <table>
            <Statistic stat={props.stat.hyvä} nimi="Hyvä"/>
            <Statistic stat={props.stat.neutraali} nimi="Neutraali"/>
            <Statistic stat={props.stat.huono} nimi="Huono"/>
-            <Keskiarvo stat={props.stat}/>
-            <Positiivisia stat={props.stat}/>
+           <Statistic stat={keskiarvo(hyvia,neutraaleja,huonoja,yhteensa)} nimi="Keskiarvo"/>
+           <Statistic stat={positiivisia(hyvia,yhteensa)}nimi="Positiivisia"/>
+           </table>
         </div>
     )
 }
 const Statistic=(props)=>{
+    if(props.nimi==="Positiivisia"){
+     return(
+         <tr>
+            <td>{props.nimi}: </td>
+            <td>{props.stat}% </td>
+        </tr>
+     )}
     return (
-        <p>{props.nimi}: {props.stat} </p>
+        <tr>
+            <td>{props.nimi}: </td>
+            <td>{props.stat} </td>
+        </tr>
     )
 }
- const Keskiarvo=(props)=>{
-    return (
-        <p>Keskiarvo: {(props.stat.hyvä*1+props.stat.neutraali*0+props.stat.huono*-1)/(props.stat.yhteensa)}</p>
-    )
-}
- const Positiivisia=(props)=>{
-    return (
-        <p>Positiivisia: {100*(props.stat.hyvä/(props.stat.yhteensa))}%</p>
-    )
-}
+function keskiarvo(a,b,c,yht) {
+return ((a*1+b*0+c*-1)/yht).toFixed(2)
+     
+ }
+ function positiivisia(a,yht){
+     return (100*(a/yht)).toFixed(2)
+ }
+
 
 const Button =({handleClick, text}) =>(
     <button onClick={handleClick}>
@@ -58,6 +72,7 @@ class App  extends React.Component{
       }
   }
   lisaaNeutraali=(arvo,yht)=>{
+      
       return ()=>{
           this.setState({neutraali : arvo})
           this.setState({yhteensa : yht})
